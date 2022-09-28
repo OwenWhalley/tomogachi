1678274a342dae9e5127161d19ef8723b9dced26
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-09-28 09:11:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+// Created by: Owen Whalley
+// Created on: 2022/09/28
+// 
+// This sets hunger to 0 on start.
 let hunger = 0
+// Created by: Owen Whalley
+// Created on: 2022/09/28
+// 
+// If A button is pressed, it increases hunger by one and shows the value on the microbit. If button B is pressed, it resets hunger and shows the value.
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         hunger = hunger + 1
